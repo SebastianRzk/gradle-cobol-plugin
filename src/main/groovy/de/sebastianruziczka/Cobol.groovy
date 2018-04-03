e380a039f1b266784dbda5121cb6670fe0d87a04
RosesTheN00b <sebastian.r.ruziczka@gmail.com>
2018-04-03 23:15:12 +0200

    initial code commit

---
+++ src/main/groovy/de/sebastianruziczka/Cobol.groovy
@@ -0,0 +1,114 @@
+package de.sebastianruziczka
+
+class CobolExtension {
+	String src_file_type = '.cbl'
+	String src_main = ''
+
+	String src_main_path = 'src/main/cobol'
+	String bin_main_path = 'build/bin/main/cobol'
+	String res_main_path = 'res/main/cobol'
+
+	def filetypePattern(){
+		'**/*' + src_file_type
+	}
+
+	def absoluteSrcMainModulePath(Project project){
+		return project.file(src_main_path + '/' + src_main + src_file_type).getParent()
+	}
+
+	def absoluteSrcMainPath(Project project){
+		return project.file(src_main_path + '/' + src_main + src_file_type).absolutePath
+	}
+
+	def absoluteBinMainPath(Project project){
+		return project.file(bin_main_path + '/' +  src_main).absolutePath
+	}
+}
+
+import org.gradle.api.*
+import org.gradle.api.tasks.*
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+import java.lang.Exception
+
+class Cobol implements Plugin<Project> {
+
+	void apply(Project project) {
+		def conf = project.extensions.create('cobol', CobolExtension)
+
+		project.task ('cobolCompile', type:Exec, dependsOn: 'cobolClean') {
+			Logger logger = LoggerFactory.getLogger('cobolCompile')
+
+			def list = []
+			def tree = project.fileTree(conf.src_main_path).include(conf.filetypePattern())
+			tree.each { File file ->
+				if (!file.path.equals(conf.absoluteSrcMainPath(project))){
+					list << file.path
+				}
+			}
+
+			doFirst {
+				if (conf.src_main.equals('')) {
+					logger.error('No main file configured!')
+					logger.error('Please specify main file in your build.gradle')
+					print conf.dump()
+					throw new Exception('No main file configured!')
+				}
+				if (!project.file(conf.bin_main_path).exists()){
+					println 'creating dirs'
+					project.file(conf.bin_main_path).mkdirs()
+				}
+				commandLine 'cobc'
+				args = (['-x','-o', conf.absoluteBinMainPath(project), '--free', conf.absoluteSrcMainPath(project)] + list)
+				workingDir = conf.absoluteSrcMainModulePath(project)
+			}
+		}
+
+		project.task ('cobolRun', type:Exec, dependsOn: ['cobolCompile', 'cobolCopyRessources']) {
+			commandLine 'gnome-terminal', '--wait', '--', conf.absoluteBinMainPath(project)
+			standardInput = System.in
+		}
+
+		project.task ('cobolCopyRessources', type: Copy){
+			doFirst {
+				project.file('bin/main/cobol').mkdirs()
+			}
+			from project.file(conf.res_main_path).absolutePath
+			into project.file(conf.bin_main_path).absolutePath
+		}
+
+		project.task ('cobolClean', type: Delete){
+			doFirst {
+				delete project.file(conf.bin_main_path).absolutePath
+			}
+		}
+
+		project.task ('cobolCompilerVersion', type: Exec){
+			commandLine = 'cobc'
+			args = ['--version']
+		}
+
+		project.task ('cobolGradleConfiguration') {
+			doFirst {
+				println '>>> Configured settings:'
+				println conf.dump()
+				println 'Computed paths:'
+				println 'absoluteSrcMainModulePath: ' + conf.absoluteSrcMainModulePath(project)
+				println 'absoluteSrcMainPath: ' + conf.absoluteSrcMainPath(project)
+				println 'absoluteBinMainPath: ' + conf.absoluteBinMainPath(project)
+				println '<<<'
+			}
+		}
+
+		project.task ('cobolConfiguration', dependsOn: ['cobolCompilerVersion', 'cobolGradleConfiguration']){
+			doFirst {
+				println 'Conf'
+			}
+		}
+
+
+	}
+}
+
