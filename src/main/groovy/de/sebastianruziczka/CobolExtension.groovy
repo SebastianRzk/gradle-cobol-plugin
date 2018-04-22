69795564881fcc3accc674d93533d7fc92a6593c
RosesTheN00b <sebastian.r.ruziczka@gmail.com>
2018-04-22 13:38:46 +0200

    made compile src-file independent

--- src/main/groovy/de/sebastianruziczka/CobolExtension.groovy
+++ src/main/groovy/de/sebastianruziczka/CobolExtension.groovy
@@ -30,16 +30,38 @@ class CobolExtension {
 	}
 
 	String absoluteSrcMainModulePath(Project project){
-		return project.file(srcMainPath + '/' + srcMain + srcFileType).getParent()
+		return this.absoluteSrcMainModulePath(project, this.srcMain)
+	}
+
+	String absoluteSrcMainModulePath(Project project, String sourceFile){
+		if (!sourceFile.endsWith(this.srcFileType)) {
+			sourceFile = sourceFile + this.srcFileType
+		}
+		return project.file(srcMainPath + '/' + sourceFile).getParent()
 	}
 
 	String absoluteSrcMainPath(Project project){
-		return project.file(srcMainPath + '/' + srcMain + srcFileType).absolutePath
+		return this.absoluteSrcMainPath(project, this.srcMain)
+	}
+
+	String absoluteSrcMainPath(Project project, String mainFile){
+		if (!mainFile.endsWith(this.srcFileType)) {
+			mainFile = mainFile + this.srcFileType
+		}
+		return project.file(srcMainPath + '/' + mainFile).absolutePath
 	}
 
 	String absoluteBinMainPath(Project project){
 		return project.file(binMainPath + '/' +  srcMain).absolutePath
 	}
+
+	String absoluteBinMainPath(Project project, String name){
+		if (name.endsWith(this.srcFileType)) {
+			name = name.substring(0, name.length() - this.srcFileType.length())
+		}
+		return project.file(binMainPath + '/' +  name).absolutePath
+	}
+
 
 	String absoluteSrcTestPath(Project project) {
 		return project.file(this.srcTestPath).absolutePath
