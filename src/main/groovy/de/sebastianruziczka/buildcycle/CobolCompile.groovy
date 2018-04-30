f8e50797e7698afbc3a01affa16ccb7f40bb967e
RosesTheN00b <sebastian.r.ruziczka@gmail.com>
2018-04-30 15:42:27 +0200

    shortname to improve compability with oder versions

--- src/main/groovy/de/sebastianruziczka/buildcycle/CobolCompile.groovy
+++ src/main/groovy/de/sebastianruziczka/buildcycle/CobolCompile.groovy
@@ -43,7 +43,7 @@ class CobolCompile {
 		def dependencies = resolveCompileDependencies(project, conf, mainFile)
 
 		def command = ['cobc']
-		command << '-verbose' //
+		command << '-v' //
 		command << '-x' // Build executable
 		command << '-o'
 		command << conf.absoluteBinMainPath(mainFile) // Executable destination path
