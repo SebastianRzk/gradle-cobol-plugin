eeb6847b0c40d244527f575a531db50530e092ec
SebastianRzk <5215946+SebastianRzk@users.noreply.github.com>
2021-01-20 16:53:17 +0100

    Update gh-name

--- ci/run_tests.sh
+++ ci/run_tests.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 set -e
 
-git clone https://github.com/RosesTheN00b/gradle-cobol-plugin-example
+git clone https://github.com/SebastianRzk/gradle-cobol-plugin-example
 
 cd gradle-cobol-plugin-example
 sh ci/run_dev_test.sh
